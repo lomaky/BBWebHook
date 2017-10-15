a6b14ddc6a55421bd3f3e424ee5a42c2fa596612
Mr.O <oscar@gensolve.com>
2017-10-15 19:15:05 +1000

    test BBWebHookData request param

--- src/main/java/com/bbwebhook/handler/BbPullRequestHandler.java
+++ src/main/java/com/bbwebhook/handler/BbPullRequestHandler.java
@@ -12,14 +12,14 @@ import com.bbwebhook.models.BBWebHookData;
 /**
  * Handler for requests to Lambda function.
  */
-public class BbPullRequestHandler implements RequestHandler<BBWebHookData, Object> {
+public class BbPullRequestHandler implements RequestHandler<Object, Object> {
 
 	@Override
-    public Object handleRequest(final BBWebHookData input, final Context context) {
+    public Object handleRequest(final Object input, final Context context) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
         
-        return new GatewayResponse(input.getPullrequest().getTitle() , headers, 200);
+        return new GatewayResponse(((BBWebHookData)input).getPullrequest().getTitle() , headers, 200);
     }
 
 }
