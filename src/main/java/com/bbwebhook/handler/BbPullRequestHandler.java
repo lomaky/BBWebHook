c288ac8f7384a7fbeb1582ea218d6d9f119bd4b3
Mr.O <oscar@gensolve.com>
2017-10-15 21:07:04 +1000

    push to sns

--- src/main/java/com/bbwebhook/handler/BbPullRequestHandler.java
+++ src/main/java/com/bbwebhook/handler/BbPullRequestHandler.java
@@ -2,6 +2,9 @@ package com.bbwebhook.handler;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.model.PublishRequest;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,6 +18,7 @@ import com.google.gson.Gson;
  */
 public class BbPullRequestHandler implements RequestHandler<Object, Object> {
 
+	@SuppressWarnings("unchecked")
 	@Override
     public Object handleRequest(Object input, Context context) {
         Map<String, String> headers = new HashMap<>();
@@ -24,13 +28,20 @@ public class BbPullRequestHandler implements RequestHandler<Object, Object> {
 			String body = mapInput.get("body");
 			Gson g = new Gson(); 
 			BBWebHookData bbWebHookData = g.fromJson(body, BBWebHookData.class);
+			
+			AmazonSNS sns = AmazonSNSClient.builder().build();
+			sns.publish(
+					new PublishRequest("arn:aws:sns:us-east-1:257530247365:GSBBAlarm",
+						body, 
+						bbWebHookData.getPullrequest().getTitle())
+					);
 			return new GatewayResponse(
 	        		"{ \"Output\": \""+bbWebHookData.getPullrequest().getTitle()+"\"}",
-	        		headers, 401);
+	        		headers, 200);
 		}catch (Exception e) {
 			return new GatewayResponse(
 	        		"{ \"Output FAIL\": \""+e.getMessage()+"\"}",
-	        		headers, 401);
+	        		headers, 400);
 		}
     }
 
